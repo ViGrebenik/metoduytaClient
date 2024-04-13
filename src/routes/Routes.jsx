27ff8146333d5a0cb2c0a718729d6cfa0f0422eb
Виталий Игоревий <vi@MacBook-Pro-Vitalij.local>
2024-04-13 20:59:54 +0300

    fixed seo

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import NotFound from '../components/screens/not-found/NotFound'
 
+import { Helmet } from 'react-helmet'
 import { useModal } from '../assets/services/ModalContext'
 import ScrollToTop from '../assets/services/ScrollToTop'
 import CalculatorUIHome from '../components/calculator/uiHome/CalculatorUIHome'
@@ -16,6 +17,23 @@ const Router = () => {
 	const { isModalOpen, closeModal } = useModal()
 	return (
 		<BrowserRouter>
+			<Helmet>
+				<title>
+					Метод Уюта | Комплексный ремонт квартир в Санкт-Петербурге
+				</title>
+				<meta
+					name='description'
+					content='Компания Метод Уюта предоставляет услуги по ремонту квартир в Санкт-Петербурге под ключ.'
+				/>
+				<meta name='keywords' content='Метод Уюта' />
+				<link rel='canonical' href='https://metod-yuta.ru' />
+				<meta property='og:title' content='Метод Уюта' />
+				<meta
+					property='og:description'
+					content='Компания Метод Уюта предоставляет услуги по ремонту квартир в Санкт-Петербурге под ключ.'
+				/>
+				<meta property='og:image' content='https://metod-yuta.ru' />
+			</Helmet>
 			{isModalOpen && <CallRequestForm onClose={closeModal} />}
 			<ScrollToTop />
 			<Header />
