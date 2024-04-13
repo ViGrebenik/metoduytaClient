0c68ec98be671e27d013256c2d9a829158ac4c49
Виталий Игоревий <vi@MacBook-Pro-Vitalij.local>
2024-04-13 21:06:18 +0300

    fixed seo

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -26,7 +26,7 @@ const Router = () => {
 					content='Компания Метод Уюта предоставляет услуги по ремонту квартир в Санкт-Петербурге под ключ.'
 				/>
 				<meta name='keywords' content='Метод Уюта' />
-				<link rel='canonical' href='https://metod-yuta.ru' />
+				<link rel='canonical' href='http://metod-yuta.ru' />
 				<meta property='og:title' content='Метод Уюта' />
 				<meta
 					property='og:description'
