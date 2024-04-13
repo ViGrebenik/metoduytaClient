8724850b11f28b53736bc3c2fe64afae42015904
Виталий Игоревий <vi@MacBook-Pro-Vitalij.local>
2024-04-13 21:12:03 +0300

    fixed seo

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import NotFound from '../components/screens/not-found/NotFound'
 
-import { Helmet } from 'react-helmet'
 import { useModal } from '../assets/services/ModalContext'
 import ScrollToTop from '../assets/services/ScrollToTop'
 import CalculatorUIHome from '../components/calculator/uiHome/CalculatorUIHome'
@@ -17,21 +16,6 @@ const Router = () => {
 	const { isModalOpen, closeModal } = useModal()
 	return (
 		<BrowserRouter>
-			<Helmet>
-				<title>
-					Метод Уюта | Комплексный ремонт квартир в Санкт-Петербурге
-				</title>
-				<meta
-					name='description'
-					content='Компания Метод Уюта предоставляет услуги по ремонту квартир в Санкт-Петербурге под ключ.'
-				/>
-				<meta property='og:title' content='Метод Уюта' />
-				<meta
-					property='og:description'
-					content='Компания Метод Уюта предоставляет услуги по ремонту квартир в Санкт-Петербурге под ключ.'
-				/>
-				<meta property='og:image' content='https://metod-yuta.ru' />
-			</Helmet>
 			{isModalOpen && <CallRequestForm onClose={closeModal} />}
 			<ScrollToTop />
 			<Header />
--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	build: {
-		rollupOptions: {
-			external: ['react-helmet']
-		}
-	}
+	plugins: [react()]
 })
